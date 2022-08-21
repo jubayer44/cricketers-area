86dc179595751079a9e20648fd88bb1d791be668
jubaer44 <jubayerahmedshamim44@gmail.com>
2022-08-22 05:18:22 +0600

    after selection 5 button than disabled all button

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,40 @@
 // let array = [];
 let count = 0;
 const cardsBtn = document.getElementsByClassName('btn-success')
-for(let btn of cardsBtn){
-    btn.addEventListener('click', function(event){
+for (let btn of cardsBtn) {
+    btn.addEventListener('click', function (event) {
         const targetValue = event.target;
         targetValue.disabled = true;
-        
-        
-       const playersName = (event.target.parentNode.children[1].children[0].innerText)
-    //    array.push(playersName);
-       count = count + 1;
-       if(count === 6){
-        alert('sorry')
-        return;
-    }
-    displayText(count, playersName)
-})
+
+
+        const playersName = (event.target.parentNode.children[1].children[0].innerText)
+        //    array.push(playersName);
+        count = count + 1;
+        //    if(count === 6){
+        //     alert('sorry')
+        //     return;
+        // }
+        displayText(count, playersName)
+    })
 };
 
+
+
 //Display selected players name:
-function displayText(num, name){
-    
+function displayText(num, name) {
+    if (num <= 5) {
         const tableBody = document.getElementById('body-table');
-       const ol = document.createElement('ol');
-       ol.innerHTML = `
+        const ol = document.createElement('ol');
+        ol.innerHTML = `
        <span>${num}.</span>
        <span>${name}</span>
        `;
-       tableBody.appendChild(ol)   
+        tableBody.appendChild(ol)
+        return
+    }
+    const allButtons = document.querySelectorAll('.btn-success');
+    for (let button of allButtons) {
+        button.disabled = true;
+    }
+
 };
--- JS/ruf.js
+++ JS/ruf.js
@@ -2,34 +2,44 @@
 // let array = [];
 let count = 0;
 const cardsBtn = document.getElementsByClassName('btn-success')
-for(let btn of cardsBtn){
-    btn.addEventListener('click', function(event){
+for (let btn of cardsBtn) {
+    btn.addEventListener('click', function (event) {
         const targetValue = event.target;
         targetValue.disabled = true;
-        
-        
-       const playersName = (event.target.parentNode.children[1].children[0].innerText)
-    //    array.push(playersName);
-       count = count + 1;
-       if(count === 6){
-        alert('sorry')
-        return;
-    }
-    displayText(count, playersName)
-})
+
+
+        const playersName = (event.target.parentNode.children[1].children[0].innerText)
+        //    array.push(playersName);
+        count = count + 1;
+        //    if(count === 6){
+        //     alert('sorry')
+        //     return;
+        // }
+        displayText(count, playersName)
+    })
 };
 
+
+
 //Display selected players name:
-function displayText(num, name){
-    
+function displayText(num, name) {
+    if (num < 6) {
         const tableBody = document.getElementById('body-table');
-       const ol = document.createElement('ol');
-       ol.innerHTML = `
+        const ol = document.createElement('ol');
+        ol.innerHTML = `
        <span>${num}.</span>
        <span>${name}</span>
        `;
-       tableBody.appendChild(ol)   
+        tableBody.appendChild(ol)
+        return
+    }
+    const allButtons = document.querySelectorAll('.btn-success');
+    for (let button of allButtons) {
+        button.disabled = true;
+    }
+
 };
+
 
 
 
