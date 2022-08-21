c4cbf067646660446fa5ea441e34aa9e4d2c4c30
jubaer44 <jubayerahmedshamim44@gmail.com>
2022-08-22 04:01:50 +0600

    set value in seleted button

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,19 @@
-let count = [];
+let array = [];
+let count = 0;
 const cardsBtn = document.getElementsByClassName('btn-success')
 for(let btn of cardsBtn){
     btn.addEventListener('click', function(event){
        const playersName = (event.target.parentNode.children[1].children[0].innerText)
-       count.push(playersName);
-       const count = count.length;
-       return playersName;
-    //    console.log(playersName);
-    })
+       array.push(playersName);
+       count = count + 1;
+    //    console.log(count);
+       const tableBody = document.getElementById('body-table');
+       const ol = document.createElement('ol');
+       ol.innerHTML = `
+       <span>${count}.</span>
+       <span>${playersName}</span>
+       `;
+       tableBody.appendChild(ol)
+       
+})
 };
--- JS/ruf.js
+++ JS/ruf.js
@@ -1,27 +1,46 @@
 
 
 
-function selectedPlayers(elementId){
-    const tableBody = document.getElementById(elementId);
-    const ol = document.createElement('ol');
-    ol.innerHTML = `
-    <li>${cardsBtn}</li>
-    `;
-    tableBody.appendChild(ol)
-    console.log(ol);
-}
-selectedPlayers('card-body')
+// function selectedPlayers(elementId){
+    
+//     console.log(ol);
+// }
+// selectedPlayers('card-body')
 
 
 
-let count = [];
-const cardsBtn = document.getElementsByClassName('btn-success')
-for(let btn of cardsBtn){
-    btn.addEventListener('click', function(event){
-       const playersName = (event.target.parentNode.children[1].children[0].innerText)
-       count.push(playersName);
-       const count = count.length;
-       return playersName;
-    //    console.log(playersName);
-    })
-};
+// let array = [];
+// let count = 0;
+// const cardsBtn = document.getElementsByClassName('btn-success')
+// for(let btn of cardsBtn){
+//     btn.addEventListener('click', function(event){
+//        const playersName = (event.target.parentNode.children[1].children[0].innerText)
+//        array.push(playersName);
+//        count = count + 1;
+//     //    console.log(count);
+//        const tableBody = document.getElementById('body-table');
+//        const ol = document.createElement('ol');
+//        ol.innerHTML = `
+//        <span>${count}.</span>
+//        <span>${playersName}</span>
+//        `;
+//        tableBody.appendChild(ol)
+       
+// })
+// };
+
+
+
+
+// let count = [];
+// const cardsBtn = document.getElementsByClassName('btn-success')
+// for(let btn of cardsBtn){
+//     btn.addEventListener('click', function(event){
+//        const playersName = (event.target.parentNode.children[1].children[0].innerText)
+//        count.push(playersName);
+//     //    const count = count.length;
+//        //    return playersName;
+//        //    console.log(playersName);
+//        console.log(count.length);
+//     })
+// };
